BlueRain-debug/es40

commit 12bcae18a246071182c11df89f6a7f76ff874ba9

ES40: Selectively revert TLB expansion - it made NetBSD very unhappy

src/AlphaCPU.cpp
@@ -402,7 +402,6 @@ CAlphaCPU::CAlphaCPU(CConfigurator* cfg, CSystem* system) : CSystemComponent(cfg
 void CAlphaCPU::init()
 {
 	memset(&state, 0, sizeof(state));
-	memset(tb_hash, -1, sizeof(tb_hash));
 
 	cpu_hz = myCfg->get_num_value("speed", true, 500000000);
 
@@ -467,7 +466,6 @@ void CAlphaCPU::ResetForSystemReset()
 	const int savedProcNum = state.iProcNum;
 
 	memset(&state, 0, sizeof(state));
-	memset(tb_hash, -1, sizeof(tb_hash));
 	state.iProcNum = savedProcNum;
 
 	cpu_hz = myCfg->get_num_value("speed", true, 500000000);
@@ -920,29 +918,30 @@ _next_instruction:
 			// currently inside PALmode. It is not certain that this means we hava an interrupt to
 			// service, but we might have. This needs to be checked.
 
-			
+
 			if (state.pal_vms) {
-			  // PALcode base is set to 0x8000; meaning OpenVMS PALcode is currently active. In this
-			  // case, our VMS PALcode replacement routines are valid, and should be used as it is
-			  // faster than using the original PALcode.
+				// PALcode base is set to 0x8000; meaning OpenVMS PALcode is currently active. In this
+				// case, our VMS PALcode replacement routines are valid, and should be used as it is
+				// faster than using the original PALcode.
 
-			  if (state.eir & state.eien & 6)
-				if (vmspal_ent_ext_int(state.eir&state.eien & 6))
-				  return;
+				if (state.eir & state.eien & 6)
+					if (vmspal_ent_ext_int(state.eir & state.eien & 6))
+						return;
 
-			  if (state.sir & state.sien & 0xfffc)
-				if (vmspal_ent_sw_int(state.sir&state.sien))
-				  return;
+				if (state.sir & state.sien & 0xfffc)
+					if (vmspal_ent_sw_int(state.sir & state.sien))
+						return;
 
-			  if (state.asten && (state.aster & state.astrr & ((1<<(state.cm+1))-1) ))
-				if (vmspal_ent_ast_int(state.aster & state.astrr & ((1<<(state.cm+1))-1) ))
-				  return;
+				if (state.asten && (state.aster & state.astrr & ((1 << (state.cm + 1)) - 1)))
+					if (vmspal_ent_ast_int(state.aster & state.astrr & ((1 << (state.cm + 1)) - 1)))
+						return;
 
-			  if (state.sir & state.sien)
-				if (vmspal_ent_sw_int(state.sir&state.sien))
-				  return;
-			} else
-	  
+				if (state.sir & state.sien)
+					if (vmspal_ent_sw_int(state.sir & state.sien))
+						return;
+			}
+			else
+
 			{
 
 				// PALcode base is set to an unsupported value. We have no choice but to transfer control
@@ -1713,7 +1712,6 @@ int CAlphaCPU::RestoreState(FILE* f)
 
 	printf("%s: %d bytes restored.\n", devid_string, (int)ss);
 
-	rebuild_tb_hash();
 	return 0;
 }
 
@@ -1749,20 +1747,9 @@ int CAlphaCPU::FindTBEntry(u64 virt, int flags)
 	int i = state.last_found_tb[t][rw];
 	if (state.tb[t][i].valid
 		&& !((state.tb[t][i].virt ^ virt) & state.tb[t][i].match_mask)
-		&& (state.tb[t][i].asm_bit || (state.tb[t][i].asn == asn)))
-		return i;
+		&& (state.tb[t][i].asm_bit || (state.tb[t][i].asn == asn)))	return i;
 
 	// Otherwise, loop through the TB entries to find a match.
-	int h = TB_HASH(virt);
-	i = tb_hash[t][h];
-	if (i >= 0 && state.tb[t][i].valid
-		&& !((state.tb[t][i].virt ^ virt) & state.tb[t][i].match_mask)
-		&& (state.tb[t][i].asm_bit || (state.tb[t][i].asn == asn)))
-	{
-		state.last_found_tb[t][rw] = i;
-		return i;
-	}
-
 	for (i = 0; i < TB_ENTRIES; i++)
 	{
 		if (state.tb[t][i].valid
@@ -1770,8 +1757,6 @@ int CAlphaCPU::FindTBEntry(u64 virt, int flags)
 			&& (state.tb[t][i].asm_bit || (state.tb[t][i].asn == asn)))
 		{
 			state.last_found_tb[t][rw] = i;
-			// Fix up the hash so next lookup is O(1)
-			tb_hash[t][TB_HASH(virt)] = i;
 			return i;
 		}
 	}
@@ -2218,21 +2203,11 @@ void CAlphaCPU::add_tb(u64 virt, u64 pte_phys, u64 pte_flags, int flags)
 
 	if (i < 0)
 	{
-		// Allocate via round-robin, evicting the old entry.
 		i = state.next_tb[t];
 		state.next_tb[t]++;
 		if (state.next_tb[t] == TB_ENTRIES)
 			state.next_tb[t] = 0;
-
-		// Remove evicted entry from hash index.
-		if (state.tb[t][i].valid)
-		{
-			int old_h = TB_HASH(state.tb[t][i].virt);
-			if (tb_hash[t][old_h] == i)
-				tb_hash[t][old_h] = -1;
-		}
 	}
-
 
 	state.tb[t][i].match_mask = match_mask;
 	state.tb[t][i].keep_mask = keep_mask;
@@ -2253,9 +2228,6 @@ void CAlphaCPU::add_tb(u64 virt, u64 pte_phys, u64 pte_flags, int flags)
 	state.tb[t][i].asn = asn;
 	state.tb[t][i].valid = true;
 	state.last_found_tb[t][rw] = i;
-
-	// Update hash index for the new entry.
-	tb_hash[t][TB_HASH(state.tb[t][i].virt)] = i;
 
 #if defined(DEBUG_TB_)
 #if defined(IDB)
@@ -2349,7 +2321,6 @@ void CAlphaCPU::tbia(int flags)
 	state.last_found_tb[t][0] = 0;
 	state.last_found_tb[t][1] = 0;
 	state.next_tb[t] = 0;
-	memset(tb_hash[t], -1, sizeof(tb_hash[t]));
 	if (t == 0) flush_data_page_cache();
 }
 
@@ -2366,15 +2337,8 @@ void CAlphaCPU::tbiap(int flags)
 	int t = (flags & ACCESS_EXEC) ? 1 : 0;
 	int i;
 	for (i = 0; i < TB_ENTRIES; i++)
-	{
-		if (!state.tb[t][i].asm_bit && state.tb[t][i].valid)
-		{
-			state.tb[t][i].valid = false;
-			int h = TB_HASH(state.tb[t][i].virt);
-			if (tb_hash[t][h] == i)
-				tb_hash[t][h] = -1;
-		}
-	}
+		state.tb[t][i].valid = false;
+
 	if (t == 0) flush_data_page_cache();
 }
 
@@ -2389,22 +2353,9 @@ void CAlphaCPU::tbis(u64 virt, int flags)
 	int t = (flags & ACCESS_EXEC) ? 1 : 0;
 	int i = FindTBEntry(virt, flags);
 	if (i >= 0)
-	{
 		state.tb[t][i].valid = false;
-		int h = TB_HASH(virt);
-		if (tb_hash[t][h] == i)
-			tb_hash[t][h] = -1;
-	}
-	if (t == 0) flush_data_page_cache();
-}
 
-void CAlphaCPU::rebuild_tb_hash()
-{
-	memset(tb_hash, -1, sizeof(tb_hash));
-	for (int t = 0; t < 2; t++)
-		for (int i = 0; i < TB_ENTRIES; i++)
-			if (state.tb[t][i].valid)
-				tb_hash[t][TB_HASH(state.tb[t][i].virt)] = i;
+	if (t == 0) flush_data_page_cache();
 }
 
 //\}

src/AlphaCPU.h
@@ -234,10 +234,7 @@
 /// Byte numer of an address in an ICache entry.
 #define ICACHE_BYTE_MASK  (u64) (ICACHE_INDEX_MASK << 2)
 /// Number of entries in each Translation Buffer
-#define TB_ENTRIES        128 // real EV68 has 128
-
-#define TB_HASH_SIZE      256
-#define TB_HASH(virt)     ((int)(((virt) >> 13) & (TB_HASH_SIZE - 1)))
+#define TB_ENTRIES        16 // real EV68 has 128
 
 /**
  * \brief Emulated CPU.
@@ -315,8 +312,6 @@ private:
 
   int             get_icache(u64 address, u32* data);
   int             FindTBEntry(u64 virt, int flags);
-  void            rebuild_tb_hash();
-  int             tb_hash[2][TB_HASH_SIZE];
   void            add_tb(u64 virt, u64 pte_phys, u64 pte_flags, int flags);
   void            add_tb_i(u64 virt, u64 pte);
   void            add_tb_d(u64 virt, u64 pte);
@@ -675,7 +670,7 @@ inline void CAlphaCPU::set_PAL_BASE(u64 pb)
 #ifdef DEBUG_PAL
   // Dump PAL scratch area contents for non-VMS PAL
   if (!state.pal_vms && state.r[53] != 0) {
-    
+
     u64 scratch = U64(0x7cf420);
 
     printf("%%CPU-I-PALSCR: Scratch area at %016" PRIx64 ":\n", scratch);

commit 53e4ca799bb241ce935beccb39f971fd0b7c1b7c

AlphaCPU: Fix TBIAP regression introduced March 3rd in 12bcae1

src/AlphaCPU.cpp
@@ -2425,7 +2425,8 @@ void CAlphaCPU::tbiap(int flags)
 	int t = (flags & ACCESS_EXEC) ? 1 : 0;
 	int i;
 	for (i = 0; i < TB_ENTRIES; i++)
-		state.tb[t][i].valid = false;
+		if (!state.tb[t][i].asm_bit)
+			state.tb[t][i].valid = false;
 
 	if (t == 0) flush_data_page_cache();
 }